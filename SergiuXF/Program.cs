f18f80c15f36a70e043cf221beb4cd55d5ae827e
Alexia <Alexia@DESKTOP-GQDM62G>
2023-11-19 21:42:49 +0200

    Add project files.

---
+++ SergiuXF/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SergiuXF
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car Ford = new Car();
+            Car Opel = new Car();
+
+            Ford.name = "Mustang";
+            Ford.color = "white";
+            Ford.year = 2021;
+            Ford.maxSpeed = 100;
+
+            Opel.name = "Astra";
+            Opel.color = "blue";
+            Opel.year = 2020;
+            Opel.maxSpeed = 120;
+
+            Console.WriteLine(Ford.name);
+            Console.WriteLine(Ford.color);
+            Console.WriteLine(Ford.year);
+            Ford.fullThrottle();
+
+            Console.WriteLine(Opel.name);
+            Console.WriteLine(Opel.color);
+            Console.WriteLine(Opel.year);
+            Opel.fullThrottle();
+
+            Console.ReadLine();
+        }
+    }
+}
